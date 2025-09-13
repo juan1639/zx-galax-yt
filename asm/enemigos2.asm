d574ba2fb13cef7269a39ca626f23f93f519afed
juane <juaneguiabad1639@gmail.com>
2025-09-13 14:17:01 +0200

    Video 9

--- asm/enemigos2.asm
+++ asm/enemigos2.asm
@@ -7,23 +7,23 @@ mover_marcianos:
 	ld	h,a
 	ld	a,(marciano_x)
 	ld	l,a
-
-	;---------------------------------------
-	ld	b,$18	; Bucle de 24 marcianos
+	
+	;-----------------------------------
+	ld	b,$18	; Bucle 24 marcianos
 
 bucle_todos_los_marcianos:
 	ld	de,marcianos_sprites
 
 	push	bc
 
-	ld	b,$08	; Bucle de 8 scanlines
+	ld	b,$08	; Bucle 8 scanlines (solo 1 caracter de alto)
 
 	bucle_dibuja_marciano:
 		ld	a,(de)
 		ld	(hl),a	; 1er byte (izquierda)
-
+	
 		inc	l
-		inc	de
+		inc 	de
 
 		ld	a,(de)
 		ld	(hl),a	; 2do byte
@@ -35,31 +35,47 @@ bucle_todos_los_marcianos:
 		ld	(hl),a	; 3er byte
 
 		dec	l
-		dec	l
+		dec 	l
 
 		inc	h
 		inc	de
 	djnz	bucle_dibuja_marciano
 
-	ld	a,h
-	sub	$08
-	ld	h,a
+		;------------------------------
+		; Attr Marciano
+		;------------------------------
+		;ld	hl,$5824
+		;ld	a,%01000100
+		;ld	(hl),a
 
-	inc	l
-	inc	l
-	inc	l
-	inc	l
+		;inc	l
+		;ld	a,%01000100
+		;ld	(hl),a
 
-	pop	bc
+		;inc	l
+		;ld	a,%01000100
+		;ld	(hl),a
+		;------------------------------
 
-	ld	a,b
-	cp	$11
-	call	z,incrementar_l
+		ld	a,h
+		sub	$08
+		ld	h,a
+		
+		inc	l
+		inc	l
+		inc 	l
+		inc	l
+		
+		pop	bc
+		ld	a,b
 
-	cp	$09
-	call	z,decrementar_l
-	
-djnz 	bucle_todos_los_marcianos
+		cp	$11
+		call	z,incrementar_l
+
+		cp	$09
+		call	z,decrementar_l
+
+djnz	bucle_todos_los_marcianos
 ret
 
 ;-------------------------------------------------
@@ -77,13 +93,3 @@ decrementar_l:
 	ld	l,a
 	ret
 
-
-
-
-
-
-
-		
-	
-
-	
